4941ef525cb8c207e8d548e492e3880c68ebe89a
canattmaca <119670826+canattmaca@users.noreply.github.com>
2023-05-12 17:04:23 +0300

    template engine,partials, routes olusturuldu

--- app.js
+++ app.js
@@ -2,8 +2,37 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//MiddleWares
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+});
+app.get('/contact', (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+});
+app.get('/courses', (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'courses',
+  });
+});
+app.get('/doashboard', (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'doashboard',
+  });
 });
 
 app.listen(port, () => {
