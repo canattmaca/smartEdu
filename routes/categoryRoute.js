66df695de204b7dd6b3f2d3dcbee5994839ed48f
canattmaca <119670826+canattmaca@users.noreply.github.com>
2023-05-21 18:44:26 +0300

    Gerekli kontroller yapıldı. Sayfalama işlemi yapılacak kurslar ve getAllCourse kısmında

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const categoryControllers = require("../controllers/categoryControllers");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(categoryControllers.createCategory); //http://localhost:3000/categories
-
-router.route("/:id").delete(categoryControllers.deleteCategory); //http://localhost:3000/categories
+router
+  .route("/")
+  .post(roleMiddleware("admin"), categoryControllers.createCategory); //http://localhost:3000/categories
+router
+  .route("/:id")
+  .delete(roleMiddleware("admin"), categoryControllers.deleteCategory); //http://localhost:3000/categories
 
 module.exports = router;
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -9,8 +9,12 @@ router
   .post(roleMiddleware(["teacher", "admin"]), courseController.createCourse); //http://localhost:3000/courses
 router.route("/").get(courseController.getAllCourses);
 router.route("/:slug").get(courseController.getCourse);
-router.route("/:slug").delete(courseController.deleteCourse);
-router.route("/:slug").put(courseController.updateCourse);
+router
+  .route("/:slug")
+  .delete(roleMiddleware(["teacher", "admin"]), courseController.deleteCourse);
+router
+  .route("/:slug")
+  .put(roleMiddleware(["teacher", "admin"]), courseController.updateCourse);
 router.route("/enroll").post(courseController.enrollCourse); //http://localhost:3000/courses/enroll
 router.route("/release").post(courseController.releaseCourse); //http://localhost:3000/courses/release
 
